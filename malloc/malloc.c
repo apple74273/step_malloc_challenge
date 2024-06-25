85e5ec43c2c0296625dc5a7f2abab286c38301aa
NahoYokoyama <na.74273.777@gmail.com>
2024-06-25 11:00:27 +0900

    add homework files

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -38,23 +38,43 @@ typedef struct my_heap_t {
 //
 // Static variables (DO NOT ADD ANOTHER STATIC VARIABLES!)
 //
-my_heap_t my_heap;
+my_heap_t my_heap[4];
 
 //
 // Helper functions (feel free to add/remove/edit!)
 //
 
+//free binのインデックスを計算する
+int find_index_of_the_bin(size_t size) {
+    int index = 0;
+    if (size < 1000) {
+        index = 0;
+    }
+    else if (size < 2000) {
+        index = 1;
+    }
+    else if (size < 3000) {
+        index = 2;
+    }
+    else {
+        index = 3;
+    }
+    return index;
+}
+
 void my_add_to_free_list(my_metadata_t *metadata) {
+  int index = find_index_of_the_bin(metadata->size);
   assert(!metadata->next);
-  metadata->next = my_heap.free_head;
-  my_heap.free_head = metadata;
+  metadata->next = my_heap[index].free_head;
+  my_heap[index].free_head = metadata;
 }
 
 void my_remove_from_free_list(my_metadata_t *metadata, my_metadata_t *prev) {
+  int index = find_index_of_the_bin(metadata->size);
   if (prev) {
     prev->next = metadata->next;
   } else {
-    my_heap.free_head = metadata->next;
+    my_heap[index].free_head = metadata->next;
   }
   metadata->next = NULL;
 }
@@ -65,9 +85,11 @@ void my_remove_from_free_list(my_metadata_t *metadata, my_metadata_t *prev) {
 
 // This is called at the beginning of each challenge.
 void my_initialize() {
-  my_heap.free_head = &my_heap.dummy;
-  my_heap.dummy.size = 0;
-  my_heap.dummy.next = NULL;
+  for (int i = 0; i < 4; i++) {
+      my_heap[i].dummy.size = 0;
+      my_heap[i].dummy.next = NULL;
+      my_heap[i].free_head = &my_heap[i].dummy;
+  }
 }
 
 // my_malloc() is called every time an object is allocated.
@@ -75,15 +97,38 @@ void my_initialize() {
 // 4000. You are not allowed to use any library functions other than
 // mmap_from_system() / munmap_to_system().
 void *my_malloc(size_t size) {
-  my_metadata_t *metadata = my_heap.free_head;
+  int index = find_index_of_the_bin(size);
+  my_metadata_t* metadata = my_heap[index].free_head;
   my_metadata_t *prev = NULL;
   // First-fit: Find the first free slot the object fits.
-  // TODO: Update this logic to Best-fit!
-  while (metadata && metadata->size < size) {
-    prev = metadata;
-    metadata = metadata->next;
+  // TODO: Update this logic to Best-fit! -> This is the implemented version
+  size_t best_difference = -1;
+  my_metadata_t *best_metadata = NULL;
+  my_metadata_t *best_prev = NULL;
+  //そのindexを探して見つからない→もう1サイズ大きいbinを探したら見つかるかも
+  while (index < 4) {
+      metadata = my_heap[index].free_head;
+      while (metadata) {
+          size_t difference = metadata->size - size;
+          if (difference >= 0) {
+              if (best_difference > difference || best_difference == -1) {
+                  best_difference = difference;
+                  best_metadata = metadata;
+                  best_prev = prev;
+              }
+          }
+          prev = metadata;
+          metadata = metadata->next;
+      }
+      if (best_difference != -1) {
+          break;
+      } else {
+          index += 1;
+      }
   }
-  // now, metadata points to the first free slot
+  metadata = best_metadata;
+  prev = best_prev;
+  // now, metadata points to the best slot
   // and prev is the previous entry.
 
   if (!metadata) {
@@ -160,4 +205,15 @@ void my_finalize() {
 void test() {
   // Implement here!
   assert(1 == 1); /* 1 is 1. That's always true! (You can remove this.) */
+  /*my_malloc(128);
+  my_malloc(128);
+  my_malloc(128);
+  my_malloc(128);
+  my_malloc(128);
+  my_malloc(128);
+  my_malloc(128);
+  my_malloc(4096);
+  my_malloc(1024);
+  my_malloc(2048);
+  printf("Succeed\n")*/
 }
